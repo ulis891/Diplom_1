69bc8661cfe35a628954e1f39e851ef96c38e564
Aleksandr Kostromin <ulis89@yandex.ru>
2025-09-25 18:54:53 +0300

    add class data ref test_bun.py

--- src/test_data.py
+++ src/test_data.py
@@ -1,3 +1,9 @@
-class TestData:
+class BumData:
     BUM_NAME = "test"
     BUM_PRICE = 100
+
+
+class IngredientData:
+    INGREDIENT_TYPE = "test Ingredient type"
+    INGREDIENT_NAME = "test Ingredient name"
+    INGREDIENT_PRICE = 150
--- tests/test_bun.py
+++ tests/test_bun.py
@@ -1,11 +1,11 @@
 import pytest
-from src.test_data import TestData
+from src.test_data import BumData
 
 
 class TestBun:
 
     def test_get_bun(self, make_bun):
-        assert make_bun.get_name() == TestData.BUM_NAME
+        assert make_bun.get_name() == BumData.BUM_NAME
 
     def test_get_bun_price(self, make_bun):
-        assert make_bun.get_price() == TestData.BUM_PRICE
+        assert make_bun.get_price() == BumData.BUM_PRICE

fd5fa6ce4fa997b32e77198c350137a5806422df
Aleksandr Kostromin <ulis89@yandex.ru>
2025-09-25 23:21:59 +0300

    add data to test_data.py

--- src/test_data.py
+++ src/test_data.py
@@ -7,3 +7,9 @@ class IngredientData:
     INGREDIENT_TYPE = "test Ingredient type"
     INGREDIENT_NAME = "test Ingredient name"
     INGREDIENT_PRICE = 150
+
+    INGREDIENTS_TEST_DATA = [
+        ("SAUCE", "Соус традиционный", 50),
+        ("SAUCE", "Соус spicy", 90),
+        ("FILLING", "Биокотлета", 500)
+    ]

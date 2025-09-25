89d4213a4ab20379821c66cca279e8ca1ea07f00
Aleksandr Kostromin <ulis89@yandex.ru>
2025-09-25 23:21:17 +0300

    add new fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 from praktikum.bun import Bun
 from praktikum.ingredient import Ingredient
 from praktikum.burger import Burger
+from praktikum.database import Database as DB
 from src.test_data import BumData as BD, IngredientData as ID
 from unittest.mock import Mock
 
@@ -22,6 +23,22 @@ def burger():
 
 
 @pytest.fixture
+def burger_with_ingredient(mock_bun, mock_ingredient):
+    burger = Burger()
+    burger.set_buns(mock_bun)
+    burger.add_ingredient(mock_ingredient)
+    return burger
+
+
+@pytest.fixture
+def burger_with_three_ingredients(burger, mock_bun):
+    burger.set_buns(mock_bun)
+    for ingredient in ID.INGREDIENTS_TEST_DATA:
+        burger.add_ingredient(ingredient)
+    return burger
+
+
+@pytest.fixture
 def mock_bun():
     bun = Mock()
     bun.get_price.return_value = BD.BUM_PRICE
@@ -36,3 +53,8 @@ def mock_ingredient():
     ingredient.get_name.return_value = ID.INGREDIENT_NAME
     ingredient.get_type.return_value = ID.INGREDIENT_TYPE
     return ingredient
+
+
+@pytest.fixture
+def db():
+    return DB()

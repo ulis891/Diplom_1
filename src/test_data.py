2cae0c163e573330934a74ab7e0825861309a4b5
Aleksandr Kostromin <ulis89@yandex.ru>
2025-09-29 18:58:07 +0300

    add data

--- src/test_data.py
+++ src/test_data.py
@@ -1,6 +1,12 @@
-class BumData:
-    BUM_NAME = "test"
-    BUM_PRICE = 100
+class BunData:
+    BUN_NAME = "test"
+    BUN_PRICE = 100
+
+    BUN_TEST_DATA = [
+        ("black bun", 100),
+        ("white bun", 200),
+        ("red bun", 300)
+    ]
 
 
 class IngredientData:
@@ -12,4 +18,29 @@ class IngredientData:
         ("SAUCE", "Соус традиционный", 50),
         ("SAUCE", "Соус spicy", 90),
         ("FILLING", "Биокотлета", 500)
+    ]
+
+
+class BurgerData(BunData, IngredientData):
+    TEST_BURGERS = [
+        {
+            "bun": BunData.BUN_TEST_DATA[0],
+            "ingredients": [IngredientData.INGREDIENTS_TEST_DATA[0]],
+            "price": 250
+        },
+        {
+            "bun": BunData.BUN_TEST_DATA[1],
+            "ingredients": [IngredientData.INGREDIENTS_TEST_DATA[1]],
+            "price": 490
+        },
+        {
+            "bun": BunData.BUN_TEST_DATA[2],
+            "ingredients": [IngredientData.INGREDIENTS_TEST_DATA[2]],
+            "price": 1100
+        },
+        {
+            "bun": BunData.BUN_TEST_DATA[0],
+            "ingredients": [IngredientData.INGREDIENTS_TEST_DATA[0], IngredientData.INGREDIENTS_TEST_DATA[1]],
+            "price": 340
+        }
     ]

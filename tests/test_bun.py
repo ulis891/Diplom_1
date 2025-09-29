cb602e130f6ad5c90dda1b69a4b2a73134ce6714
Aleksandr Kostromin <ulis89@yandex.ru>
2025-09-29 18:56:30 +0300

    fix test_bun.py and add allure description

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -1,11 +1,19 @@
-import pytest
-from src.test_data import BumData
+import allure
+from src.test_data import BunData as BD
 
 
+@allure.feature("Тестирование класса Bun")
 class TestBun:
 
-    def test_get_bun(self, make_bun):
-        assert make_bun.get_name() == BumData.BUM_NAME
+    @allure.story("Тестирование инициализации класса Bun")
+    def test_init_bun(self, make_bun):
+        assert make_bun.name == BD.BUN_NAME
+        assert make_bun.price == BD.BUN_PRICE
 
+    @allure.story("Тестирование метода get_name класса Bun")
+    def test_get_bun(self, make_bun):
+        assert make_bun.get_name() == BD.BUN_NAME
+
+    @allure.story("Тестирование метода get_price класса Bun")
     def test_get_bun_price(self, make_bun):
-        assert make_bun.get_price() == BumData.BUM_PRICE
+        assert make_bun.get_price() == BD.BUN_PRICE

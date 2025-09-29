333c5910b63be471f403635409146a7f2cc93f89
Aleksandr Kostromin <ulis89@yandex.ru>
2025-09-29 18:58:41 +0300

    add allure description to test_database.py

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,17 +1,20 @@
-import pytest
 from praktikum.database import Database
 from praktikum.bun import Bun
 from praktikum.ingredient import Ingredient
+import allure
 
 
+@allure.feature("Тестирование базы данных")
 class TestDatabase:
 
+    @allure.story("Тестирование доступных булочек")
     def test_available_buns(self, db: Database):
         buns = db.available_buns()
         assert isinstance(buns, list)
         for bun in buns:
             assert isinstance(bun, Bun)
 
+    @allure.story("Тестирование доступных ингредиентов")
     def test_avalible_ingredients(self, db: Database):
         ingredients = db.available_ingredients()
         assert isinstance(ingredients, list)
